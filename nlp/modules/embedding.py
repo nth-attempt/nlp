62c2e4f8ccf1c1b20ed148191a619d8744a75336
Karthik K <k.r.karthik@gmail.com>
2021-01-21 14:39:49 -0500

    embedding+rnn encoder

---
+++ nlp/modules/embedding.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from nlp.constants import PAD
+
+class Embedding(nn.Module):
+    
+    def __init__(
+        self,
+        vocab_size: int,
+        embedding_size: int,
+        freeze_weights: bool = False,
+        filepath: str = None,
+    ):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=PAD)
+        self.__init_weights(freeze_weights, filepath)
+        
+    def __init_weights(
+        self, 
+        freeze_weights=False, 
+        filepath=None
+    ):
+        
+        if not filepath:
+            initrange = 1.0
+            nn.init.uniform_(self.embedding.weight, -initrange, initrange)
+        else:
+            if not os.path.exists(filepath):
+                raise FileNotFoundError(f"{filepath} not found")
+            try:
+                self.load_numpy(filepath)
+            except:
+                try:
+                    self.load_torch(filepath)
+                except IOError:
+                    raise IOError(f"could not read {filepath} using numpy.load and load_state_dict(torch.load)")
+        self.embedding.weight.requires_grad = not freeze_weights
+        
+    def forward(
+        self,
+        x
+    ):
+        embedded = self.embedding(x)
+        return embedded
+    
+    def save_torch(
+        self,
+        filepath,
+    ):
+        torch.save(self.embedding.state_dict(), filepath)
+        
+    def load_torch(
+        self,
+        filepath,
+    ):
+        self.embedding.load_state_dict(torch.load(filepath))
+        
+    def save_numpy(
+        self,
+        filepath,
+    ):
+        np_embedding = self.embedding.weight.detach().cpu().numpy()
+        np.save(filepath, np_embedding)
+        
+    def load_numpy(
+        self,
+        filepath,
+    ):
+        pretrained_embedding = np.load(filepath)
+        self.embedding.weight.data.copy_(torch.from_numpy(pretrained_embedding))
--- nlp/modules/seq2seq_encoders/rnn_encoder.py
+++ nlp/modules/seq2seq_encoders/rnn_encoder.py
@@ -15,34 +15,68 @@ class RNNSeq2SeqEncoder(nn.Module):
         hidden_size,
         rnn_type="LSTM",
         num_layers=1,
-        dropout=0,
+        dropout=0.0,
         bidirectional=True,
+        freeze_weights=False,
+        filepath=None,
+        batch_first=True,
     ):
+        self.bidirectional = bidirectional
+        self.batch_first = batch_first
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        
+        self.length_dim = int(self.batch_first)
+        
         super(RNNSeq2SeqEncoder, self).__init__()
+        
         self.rnn = getattr(nn, rnn_type)(
             input_size=input_size,
             hidden_size=hidden_size,
             num_layers=num_layers,
-            batch_first=True,
+            batch_first=self.batch_first,
             dropout=dropout,
-            bidirectional=bidirectional,
+            bidirectional=self.bidirectional,
         )
-
-    def forward(self, inputs, input_lengths):
-        """
-        https://pytorch.org/docs/stable/notes/faq.html#pack-rnn-unpack-with-data-parallelism
-
-        inputs: (batch_size, seq_len, input_size)
-        input_lens: (batch_size)
-        """
-        total_length = inputs.size(1)
-        packed_inputs = pack_padded_sequence(
-            inputs, input_lengths, batch_first=True
-        )
-        packed_outputs, _ = self.rnn(packed_inputs)
-        outputs, _ = pad_packed_sequence(
-            packed_outputs, batch_first=True, total_length=total_length,
-        )
-        # outputs: (batch_size, seq_len, hidden_size)
-        return outputs
-
+        
+        self.__init_weights(freeze_weights, filepath)
+        
+    def __init_weights(
+        self,
+        freeze_weights=False,
+        filepath=None,
+    ):
+        if filepath:
+            self.load_torch(filepath)
+            
+        for param in self.rnn.parameters():
+            param.requires_grad = not freeze_weights
+            
+    def forward(
+        self,
+        inputs,
+        input_lengths,
+        h0=None, # allow for passing some learnable h0/c0
+        c0=None,
+    ):
+        total_length = inputs.size(self.length_dim)
+        if h0==None:
+            if self.batch_first:
+                h0 = torch.zeros((inputs.shape[0], (1+int(self.bidirectional))*self.num_layers, self.hidden_size), device=inputs.device)
+                c0 = torch.zeros((inputs.shape[0], (1+int(self.bidirectional))*self.num_layers, self.hidden_size), device=inputs.device)
+            else:
+                h0 = torch.zeros(((1+int(self.bidirectional))*self.num_layers, inputs.shape[1], self.hidden_size), device=inputs.device)
+                c0 = torch.zeros(((1+int(self.bidirectional))*self.num_layers, inputs.shape[1], self.hidden_size), device=inputs.device)
+                       
+        packed_inputs = pack_padded_sequence(inputs, input_lengths, batch_first=self.batch_first)
+        packed_outputs, hiddens = self.rnn(packed_inputs, (h0, c0))
+        outputs, _ = pad_packed_sequence(packed_outputs, batch_first=self.batch_firstm total_length=total_length)
+        
+        return outputs, hiddens
+    
+    def save_torch(self, filepath):
+        torch.save(self.rnn.state_dict(), filepath)
+        
+    def load_torch(self, filepath):
+        self.rnn.load_state_dict(torch.load(filepath))
+        
